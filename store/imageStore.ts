273a6ff8c2e7cf6b7d14fd38d066101a83317051
RohanADev01 <rohan.agarwal@buildclub.com.au>
2024-09-14 03:50:13 +1000

    add image store clear images and set iamge

--- store/imageStore.ts
+++ store/imageStore.ts
@@ -1,8 +1,10 @@
 import { create } from 'zustand';
 
 type ImageStore = {
+  clearImages(): unknown;
   images: string[];
   addImage: (imageUrl: string) => void;
+  setImage: (image: string[]) => void;
 };
 
 export const useImageStore = create<ImageStore>((set) => ({
@@ -11,4 +13,6 @@ export const useImageStore = create<ImageStore>((set) => ({
     set((state) => ({
       images: [...state.images, imageUrl],
     })),
+  setImage: (image: string[]) => set({ images: image }),
+  clearImages: () => set({ images: [] }),
 }));
