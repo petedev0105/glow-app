0ff14c4ba7cd01d63b36344d2681a879aa6cfe9d
RohanADev01 <rohan.agarwal@buildclub.com.au>
2024-09-15 20:41:51 +1000

    reroute to dashboard

--- components/GlowResultScreen.tsx
+++ components/GlowResultScreen.tsx
@@ -391,7 +391,7 @@ const GlowResultScreen = () => {
             <TouchableOpacity
               style={localStyles.backButton}
               // TODO NAVIGATE TO DASHBOARD HOME SCREEN
-              onPress={() => router.replace('/(auth)/welcome')}
+              onPress={() => router.replace('/(home)/dashboard')}
             >
               <Ionicons name='arrow-back' size={24} color='black' />
             </TouchableOpacity>
