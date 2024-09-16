e9eb0b6b7e46cdf4d1b20290dcf1c8d2dfe6bca8
Pete Tran <kiettran255@gmail.com>
2024-09-16 16:45:38 +1000

    export default

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -3,24 +3,24 @@ import { Stack } from "expo-router";
 const Layout = () => {
   return (
     <Stack>
-      <Stack.Screen name='start' options={{ headerShown: false }} />
-      <Stack.Screen name='next-screen' options={{ headerShown: false }} />
-      <Stack.Screen name='results-screen' options={{ headerShown: false }} />
+      <Stack.Screen name="start" options={{ headerShown: false }} />
+      <Stack.Screen name="next-screen" options={{ headerShown: false }} />
+      <Stack.Screen name="results-screen" options={{ headerShown: false }} />
       <Stack.Screen
-        name='glow-results-screen'
+        name="glow-results-screen"
         options={{ headerShown: false }}
       />
       <Stack.Screen
-        name='unlock-results-screen'
+        name="unlock-results-screen"
         options={{ headerShown: false }}
       />
       <Stack.Screen
-        name='facial-analysis-screen'
+        name="facial-analysis-screen"
         options={{ headerShown: false }}
       />
-      <Stack.Screen name='welcome' options={{ headerShown: false }} />
-      <Stack.Screen name='sign-up' options={{ headerShown: false }} />
-      <Stack.Screen name='sign-in' options={{ headerShown: false }} />
+      <Stack.Screen name="welcome" options={{ headerShown: false }} />
+      <Stack.Screen name="sign-up" options={{ headerShown: false }} />
+      <Stack.Screen name="sign-in" options={{ headerShown: false }} />
     </Stack>
   );
 };
